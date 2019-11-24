91e0e7892414e1a79853181720d19e1dd332bae8
seanchok <seanchok@gmail.com>
2019-11-25 01:53:31 +0800

    add additional fields for user profile updates; add toast controllers

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
 import { StorageProvider } from "../../providers/storage/storage";
-import {FormControl, FormGroup, FormBuilder, Validators} from "@angular/forms";
+import { FormControl, FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { TransactionHomePage } from '../transaction-home/transaction-home';
+import { cloneDeep, isEqual } from 'lodash';
+import firebase from "firebase";
+import { TranslateConfigService } from "../../providers/translation/translate-config.service";
 
 /**
  * Generated class for the UserProfilePage page.
@@ -19,26 +22,58 @@ import { TransactionHomePage } from '../transaction-home/transaction-home';
 
 export class UserProfilePage {
   user: any;
+  oldUser: any;
   formUser: FormGroup;
-  showSubmitButton: boolean;
+  submitButton: boolean;
+  listOfBType: String[] = [];
+  listOfCurrency: String[] = [];
+  listOfLang: String[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private sp: StorageProvider, private formBuilder: FormBuilder) {
+  constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      private sp: StorageProvider,
+      private formBuilder: FormBuilder,
+      private toastCtrl: ToastController,
+      private translateConfigService: TranslateConfigService,
+  ) {
     this.user = {};
-    this.showSubmitButton = false;
+    this.oldUser = {};
+    this.submitButton = false;
+    this.loadDropDowns();
     this.formUser = this.formBuilder.group({
       business_name: new FormControl('', Validators.required),
       business_address: new FormControl('', Validators.required),
       owner_name: new FormControl('', Validators.required),
       businesstype: new FormControl('', Validators.required),
       ph_no: new FormControl('', Validators.required),
+      currency: new FormControl('', Validators.required),
+      cash_balance: new FormControl(0, Validators.required),
+      discount: new FormControl(0, Validators.required),
       taxrate: new FormControl(0, Validators.required),
+      language: new FormControl('', Validators.required),
     })
   }
-
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserProfilePage');
     this.getUser();
+    this.oldUser = cloneDeep(this.user);
+  }
+
+  loadDropDowns(){
+    firebase.firestore().collection("sign-up").get()
+        .then((doc) => {
+          doc.docs[0].data().businessType.forEach((b)=>{
+            this.listOfBType.push(b);
+          })
+          doc.docs[0].data().currency.forEach((c)=>{
+            this.listOfCurrency.push(c);
+          })
+          doc.docs[0].data().language.forEach((l)=>{
+            this.listOfLang.push(l);
+          })
+        })
   }
 
   getUser() {
@@ -52,6 +87,12 @@ export class UserProfilePage {
   setUser() {
     if (!this.formUser.valid) {
       console.log('invalid user update');
+      const message = this.translateConfigService.getTranslatedMessage('Incomplete');
+      this.toastCtrl.create({
+        // @ts-ignore
+        message: message.value,
+        duration: 1000,
+      }).present();
     } else {
       this.sp.storageReady().then(() => {
         this.sp.setUserDat(this.user).then(() => {
@@ -61,7 +102,17 @@ export class UserProfilePage {
           console.error(error);
         });
       })
+      const message = this.translateConfigService.getTranslatedMessage('Update profile successful');
+      this.toastCtrl.create({
+        // @ts-ignore
+        message: message.value,
+        duration: 2000,
+      }).present();
     }
+  }
+
+  onChange() {
+    this.submitButton = !isEqual(this.user, this.oldUser);
   }
 
 }
