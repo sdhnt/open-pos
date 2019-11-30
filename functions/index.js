bf6c3b3a8cec4549ba672eb6242bd7e45bd98162
seanchok <seanchok@gmail.com>
2019-11-30 16:39:03 +0800

    create update transaction functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,32 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
+const { FirebaseDB } = require('./firestore');
 
-// The Firebase Admin SDK to access the Firebase Realtime Database.
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+exports.createUserArchive = functions.firestore.document('users/{id}').onCreate(async (snap, context) => {
+  const newUser = snap.data();
+  FirebaseDB.collection('user-archive').doc(context.params.id).set(newUser);
+});
+
+exports.updateUserArchive = functions.firestore.document('users/{id}').onUpdate(async (change, context) => {
+  const newUser = change.after.data();
+  const userRef = await FirebaseDB.collection('users').doc(context.params.id);
+  const userInArchiveRef = await FirebaseDB.collection('users-archive').doc(context.params.id);
+
+  // limit number of transactions in user collection, store extra transactions in user-archive collection
+  const userInArchive = userInArchiveRef.get();
+  const archiveLength = userInArchive.transactions.length;
+  userInArchive.transactions.splice(Math.max(archiveLength - 100, 0), Math.min(100, archiveLength));
+  userInArchive.transactions.concat(newUser.transactions);
+
+  const userLength = newUser.transactions.length;
+  if (userLength > 100) {
+    newUser.transactions.splice(0, userLength - 100);
+  } else if (userLength < 100) {
+    newUser.transactions = userInArchive.transactions.slice(Math.max(userLength - 100, 0));
+  }
+
+  userRef.update(newUser);
+  userInArchiveRef.update(userInArchive);
+});
