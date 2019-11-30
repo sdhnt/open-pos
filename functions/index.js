9c688a8cd518bbc25b0a09c5b8ac558843ea18cf
seanchok <seanchok@gmail.com>
2019-11-30 18:22:18 +0800

    fix cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,36 @@
-const { FirebaseDB } = require('./firestore');
-
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 exports.createUserArchive = functions.firestore.document('users/{id}').onCreate(async (snap, context) => {
   const newUser = snap.data();
-  FirebaseDB.collection('user-archive').doc(context.params.id).set(newUser);
+  db.collection('users-archive').doc(context.params.id).set(newUser);
 });
 
 exports.updateUserArchive = functions.firestore.document('users/{id}').onUpdate(async (change, context) => {
   const newUser = change.after.data();
-  const userRef = await FirebaseDB.collection('users').doc(context.params.id);
-  const userInArchiveRef = await FirebaseDB.collection('users-archive').doc(context.params.id);
+  console.log(newUser);
+  const userRef = await db.collection('users').doc(context.params.id);
+  const userInArchiveRef = await db.collection('users-archive').doc(context.params.id);
 
   // limit number of transactions in user collection, store extra transactions in user-archive collection
-  const userInArchive = userInArchiveRef.get();
-  const archiveLength = userInArchive.transactions.length;
-  userInArchive.transactions.splice(Math.max(archiveLength - 100, 0), Math.min(100, archiveLength));
-  userInArchive.transactions.concat(newUser.transactions);
+  userInArchiveRef.get().then(doc => {
+    const userInArchive = doc.data();
+    console.log(userInArchive);
+    const archiveLength = userInArchive.transactions.length;
+    userInArchive.transactions.splice(Math.max(archiveLength - 100, 0), Math.min(100, archiveLength));
+    userInArchive.transactions.concat(newUser.transactions);
+    console.log(userInArchive);
 
-  const userLength = newUser.transactions.length;
-  if (userLength > 100) {
-    newUser.transactions.splice(0, userLength - 100);
-  } else if (userLength < 100) {
-    newUser.transactions = userInArchive.transactions.slice(Math.max(userLength - 100, 0));
-  }
+    const userLength = newUser.transactions.length;
+    if (userLength > 100) {
+      newUser.transactions.splice(0, userLength - 100);
+    } else if (userLength < 100) {
+      newUser.transactions = userInArchive.transactions.slice(Math.max(userLength - 100, 0));
+    }
 
-  userRef.update(newUser);
-  userInArchiveRef.update(userInArchive);
+    userRef.update(newUser);
+    userInArchiveRef.update(userInArchive);
+  });
 });
