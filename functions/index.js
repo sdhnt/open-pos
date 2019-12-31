a4a68243d435085db17ff24a7b5626c0a73408ae
seanchok <seanchok@gmail.com>
2019-12-31 14:41:00 +0800

    fix default credentials bug; add userInArchive remove if user does not exist in users

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
+const serviceAccount = require("./serviceAccountKey.json");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://open-fintech.firebaseio.com",
+});
 const db = admin.firestore();
 
 exports.createUserArchive = functions.firestore.document("users/{id}").onCreate(async (snap, context) => {
@@ -33,24 +37,40 @@ exports.syncTransactions = functions.pubsub
               .doc(doc.id)
               .get();
           }
-          // const userInArchive = snapshotInArchive.data();
-          // const archiveLength = userInArchive.transactions.length;
-          // userInArchive.transactions.splice(Math.max(archiveLength - limit, 0), Math.min(limit, archiveLength));
-          // userInArchive.transactions = userInArchive.transactions.concat(user.transactions);
-          //
-          // const userLength = user.transactions.length;
-          // if (userLength > limit) {
-          //   user.transactions.splice(0, userLength - limit);
-          // } else if (userLength < limit) {
-          //   user.transactions = userInArchive.transactions.slice(Math.max(userLength - limit, 0));
-          // }
-          //
-          // db.collection("users")
-          //   .doc(doc.id)
-          //   .update(user);
-          // db.collection("users-archive")
-          //   .doc(doc.id)
-          //   .update(userInArchive);
+          const userInArchive = snapshotInArchive.data();
+          const archiveLength = userInArchive.transactions.length;
+          userInArchive.transactions.splice(Math.max(archiveLength - limit, 0), Math.min(limit, archiveLength));
+          userInArchive.transactions = userInArchive.transactions.concat(user.transactions);
+
+          const userLength = user.transactions.length;
+          if (userLength > limit) {
+            user.transactions.splice(0, userLength - limit);
+          } else if (userLength < limit) {
+            user.transactions = userInArchive.transactions.slice(Math.max(userLength - limit, 0));
+          }
+
+          db.collection("users")
+            .doc(doc.id)
+            .update(user);
+          db.collection("users-archive")
+            .doc(doc.id)
+            .update(userInArchive);
+        });
+      });
+
+    db.collection("users-archive")
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(async doc => {
+          const userRef = await db.collection("users").doc(doc.id);
+          const snapshotInArchive = await userRef.get();
+          if (!snapshotInArchive.exists) {
+            console.log(`remove user of id: ${doc.id} in archive`);
+            await db
+              .collection("users-archive")
+              .doc(doc.id)
+              .delete();
+          }
         });
       });
   });
