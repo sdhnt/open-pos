e318ed3d394a298322e5e86573516b7795bad925
Pranay Agarwal <pranayagarwal1208@gmail.com>
2020-01-06 12:57:12 +0530

    Embed youtube videos

--- src/pages/coach-goals/coach-goals.ts
+++ src/pages/coach-goals/coach-goals.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { TranslateConfigService } from "../../providers/translation/translate-config.service";
+import firebase from "firebase";
+import { DomSanitizer } from "@angular/platform-browser";
 
 /**
  * Generated class for the CoachGoalsPage page.
@@ -15,13 +17,27 @@ import { TranslateConfigService } from "../../providers/translation/translate-co
   templateUrl: "coach-goals.html",
 })
 export class CoachGoalsPage {
+  videoLinks;
   constructor(
     public navCtrl: NavController,
     private translateConfigService: TranslateConfigService,
     public navParams: NavParams,
-  ) {}
+    private dom: DomSanitizer,
+  ) {
+    firebase
+      .firestore()
+      .collection("contact-us")
+      .get()
+      .then(doc => {
+        this.videoLinks = doc.docs[1].data().video;
+      });
+  }
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad CoachGoalsPage");
+  }
+
+  linkValue(val: string){
+    return this.dom.bypassSecurityTrustResourceUrl(val);
   }
 }
