b7e9a83769b9c67c648610203b2a923012f33689
Pranay Agarwal <pranayagarwal1208@gmail.com>
2020-01-07 11:35:54 +0530

    Added notifications to coach

--- src/pages/coach-coach/coach-coach.ts
+++ src/pages/coach-coach/coach-coach.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { TranslateConfigService } from "../../providers/translation/translate-config.service";
+import firebase from 'firebase';
 
 /**
  * Generated class for the CoachCoachPage page.
@@ -15,13 +16,67 @@ import { TranslateConfigService } from "../../providers/translation/translate-co
   templateUrl: "coach-coach.html",
 })
 export class CoachCoachPage {
+  notifList = [];
   constructor(
     public navCtrl: NavController,
     private translateConfigService: TranslateConfigService,
     public navParams: NavParams,
-  ) {}
+  ) {
+    firebase
+      .firestore()
+      .collection("notifications")
+      .get()
+      .then(doc => {
+        doc.docs.forEach(element => {
+          let temp = element.data();
+          temp.date = this.getDateTime(parseInt(temp.date));
+          this.notifList.push(temp);
+        });
+        console.log(this.notifList);
+      });
+  }
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad CoachCoachPage");
+  }
+
+  getDateTime(datetime) {
+    //return (datetime.getDate() + "/" + (datetime.getMonth() + 1) + "/" + datetime. getFullYear())
+    const temp = new Date(datetime);
+    //console.log(temp);
+    const temp1 = temp;
+
+    const t =
+      temp.getDate().toString() +
+      "/" +
+      (temp.getMonth() + 1).toString() +
+      "/" +
+      temp.getFullYear().toString() +
+      " " +
+      this.getHours(temp) +
+      ":" +
+      this.getMinutes(temp);
+    return t;
+    //if any hours or mins <0 then need to add 0 4 use cases
+  }
+
+  getHours(datetime) {
+    const temp = new Date(datetime);
+    const t = temp.getHours();
+    if (t > 9) {
+      return t.toString();
+    } else {
+      return "0" + t.toString();
+    }
+  }
+
+  getMinutes(datetime) {
+    const temp = new Date(datetime);
+    const t = temp.getMinutes();
+    if (t > 9) {
+      return t.toString();
+    } else {
+      return "0" + t.toString();
+    }
   }
 }
