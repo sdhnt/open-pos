9f959f476a102ebc5631bfa20bec297d32f58496
Pranay Agarwal <pranayagarwal1208@gmail.com>
2020-01-21 12:23:16 +0530

    reprint for previous receipts

--- src/pages/summary-home/summary-home.ts
+++ src/pages/summary-home/summary-home.ts
@@ -1,8 +1,11 @@
 import { Component, NgZone } from "@angular/core";
-import { IonicPage, NavController, NavParams, ToastController, Events, Tabs } from "ionic-angular";
+import { IonicPage, NavController, NavParams, ToastController, Events, Tabs, AlertController, LoadingController } from "ionic-angular";
 import { StorageProvider } from "../../providers/storage/storage";
 import Moment from "moment";
 import { TranslateConfigService } from "../../providers/translation/translate-config.service";
+import EscPosEncoder from "esc-pos-encoder-ionic";
+import { commands } from "./../../providers/printer/printer-commands";
+import { PrinterProvider } from "../../providers/printer/printer";
 
 /**
  * Generated class for the SummaryHomePage page.
@@ -25,6 +28,9 @@ export class SummaryHomePage {
     public events: Events,
     public toastCtrl: ToastController,
     public zone: NgZone,
+    private alertCtrl: AlertController,
+    private printer: PrinterProvider,
+    private loadCtrl: LoadingController
   ) {
     this.events.subscribe("ViewRecs", data => {
       (this.navCtrl.parent as Tabs).select(2);
@@ -37,6 +43,35 @@ export class SummaryHomePage {
   tsmonth = 0;
   ts30 = 0;
   usrchoice = "today";
+
+  userdata: any = {
+    business_address: "",
+    business_name: "",
+    cash_balance: "",
+    currency: "",
+    created: "",
+    language: "en",
+    owner: "",
+    owner_name: "",
+    ph_no: "",
+    businesstype: "",
+    taxrate: 0.0,
+    discount: 0.0,
+  };
+
+  async getUserData() {
+    this.sp.storageReady().then(() => {
+      this.sp
+        .getUserDat()
+        .then(val => {
+          this.userdata = JSON.parse(val);
+          console.log(this.userdata);
+        })
+        .catch(err => {
+          alert("Error: " + err);
+        });
+    });
+  }
 
   toggleExpanded() {
     console.log("changing: " + this.expanded);
@@ -67,6 +102,7 @@ export class SummaryHomePage {
     //@ts-ignore
     //this.expandedvar = this.translateConfigService.getTranslatedMessage("Expand").value;
     this.getTransac();
+    this.getUserData();
   }
 
   getDateTime(datetime) {
@@ -223,5 +259,251 @@ export class SummaryHomePage {
       }, 1000);
       this.ionViewDidLoad();
     });
+  }
+
+  printRec(transac){
+    console.log(transac);
+    const encoder = new EscPosEncoder();
+    const result = encoder.initialize();
+
+    result
+      .codepage("cp936")
+      .align("center")
+      .raw(commands.TEXT_FORMAT.TXT_4SQUARE)
+      .line(this.userdata.business_name)
+      .raw(commands.TEXT_FORMAT.TXT_NORMAL)
+      .line(this.userdata.business_address)
+      .line(this.userdata.businesstype)
+      .line(this.userdata.ph_no)
+      .align("left")
+      .newline()
+      .line(this.getDateTime(transac.datetime))
+      .align("center")
+      .text(commands.HORIZONTAL_LINE.HR_58MM)
+      .newline();
+
+    transac.itemslist.forEach((element)=>{
+      element.qty = element.qty.toString();
+      element.price = element.price.toString();
+      //autotab system
+      if (element.name.length < 20) {
+        for (let i = element.name.length; i < 20; i++) {
+          element.name += " ";
+        }
+      } else {
+        element.name = element.name.substring(0, 20);
+      }
+
+      if (element.qty < 10000) {
+        for (let i = element.qty.length; i < 4; i++) {
+          element.qty += " ";
+        }
+      } else {
+        element.qty.substring(0, 4);
+      }
+
+      if (element.price < 10000000) {
+        for (let i = element.price.length; i < 8; i++) {
+          element.price += " ";
+        }
+      } else {
+        element.price.substring(0, 8);
+      }
+      result
+
+        .text(element.name) //19 + space
+        //.raw(commands.FEED_CONTROL_SEQUENCES.CTL_HT)
+        .text(element.qty) //4+ space
+        //.raw(commands.FEED_CONTROL_SEQUENCES.CTL_HT)
+        .text(element.price) //7+space
+
+        .newline();
+      if (parseFloat(element.discount) != 0) {
+        result
+          .text("Discount (" + Math.round(parseFloat(element.discount) * 100) / 100 + "%) : ", 30)
+          .raw(commands.FEED_CONTROL_SEQUENCES.CTL_HT)
+          .text("")
+          .raw(commands.FEED_CONTROL_SEQUENCES.CTL_HT)
+          .text("-" + Math.round((element.price * element.discount * element.qty) / 100))
+          .newline();
+      }
+    });
+    result.newline();
+    result.align("right").line("Total: " + transac.totalsum);
+    if (transac.totalsum != transac.totaldisc) {
+      result.line(" After Discount (" + Math.round(transac.discount * 100) / 100 + "%): " + transac.totaldisc);
+    }
+    if (transac.totalsum != transac.totalatax) {
+      result.line("After Tax (" + Math.round(transac.taxrate * 100) / 100 + "%): " + transac.totalatax);
+    }
+    result
+      .raw(commands.TEXT_FORMAT.TXT_4SQUARE)
+      .newline()
+      .line("")
+      .newline()
+      .line("")
+      .newline()
+      .cut("full");
+
+    this.mountAlertBt(result.encode());
+  }
+
+  showToast(data) {
+    const toast = this.toastCtrl.create({
+      duration: 3000,
+      message: data,
+      position: "bottom",
+    });
+    toast.present();
+  }
+
+  receipt: any;
+  
+  mountAlertBt(data) {
+    this.receipt = data;
+    console.log(this.receipt);
+    const msg1 = this.translateConfigService.getTranslatedMessage("Select your printer");
+    const msg2 = this.translateConfigService.getTranslatedMessage("Cancel");
+    const msg3 = this.translateConfigService.getTranslatedMessage("Select printer");
+    const msg4 = this.translateConfigService.getTranslatedMessage("Select a printer!");
+    const msg5 = this.translateConfigService.getTranslatedMessage(
+      "There was an error connecting the printer, please try again!",
+    );
+    const msg6 = this.translateConfigService.getTranslatedMessage("Error activating bluetooth, please try again!");
+
+    const alert = this.alertCtrl.create({
+      //TRANSLATE THIS
+      // @ts-ignore
+      title: msg1.value,
+      buttons: [
+        {
+          // @ts-ignore
+          text: msg2.value,
+          role: "cancel",
+        },
+        {
+          // @ts-ignore
+          text: msg3.value,
+          handler: device => {
+            if (!device) {
+              // @ts-ignore
+              this.showToast(msg4.value);
+              return false;
+            }
+            console.log(device);
+            this.print(device, this.receipt);
+          },
+        },
+      ],
+    });
+
+    this.printer
+      .enableBluetooth()
+      .then(() => {
+        this.printer
+          .searchBluetooth()
+          .then(devices => {
+            devices.forEach(device => {
+              //console.log("Devices: ", JSON.stringify(device));
+              alert.addInput({
+                name: "printer",
+                value: device.address,
+                label: device.name,
+                type: "radio",
+              });
+            });
+            alert.present();
+          })
+          .catch(error => {
+            console.log(error);
+            // @ts-ignore
+            this.showToast(msg5.value);
+            this.mountAlertBt(this.receipt);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+        // @ts-ignore
+        this.showToast(msg6.value);
+        this.mountAlertBt(this.receipt);
+      });
+  }
+
+  print(device, data) {
+    console.log("Device mac: ", device);
+    console.log("Data: ", JSON.stringify(data));
+    
+
+    const msg1 = this.translateConfigService.getTranslatedMessage("Printing...");
+    const msg2 = this.translateConfigService.getTranslatedMessage("Successful print!");
+    const msg3 = this.translateConfigService.getTranslatedMessage("Ok");
+    const msg4 = this.translateConfigService.getTranslatedMessage("There was an error printing, please try again!");
+    const load = this.loadCtrl.create({
+      // @ts-ignore
+      content: msg1.value,
+      enableBackdropDismiss: true,
+    });
+    load.present();
+    this.printer.connectBluetooth(device).subscribe(
+      status => {
+        console.log(status);
+        this.printer
+          .printData(data)
+          .then(printStatus => {
+            console.log(printStatus);
+            const alert = this.alertCtrl.create({
+              //@ts-ignore
+              title: msg2.value,
+              buttons: [
+                {
+                  //@ts-ignore
+                  text: msg3.value,
+                  handler: () => {
+                    load.dismiss();
+                    this.printer.disconnectBluetooth();
+                  },
+                },
+              ],
+            });
+            alert.present();
+            (this.navCtrl.parent as Tabs).select(0);
+          })
+          .catch(error => {
+            console.log(error);
+            const alert = this.alertCtrl.create({
+              //@ts-ignore
+              title: msg4.value,
+              buttons: [
+                {
+                  //@ts-ignore
+                  text: msg3.value,
+                  handler: () => {
+                    load.dismiss();
+                    //this.printer.disconnectBluetooth();
+                  },
+                },
+              ],
+            });
+            alert.present();
+            (this.navCtrl.parent as Tabs).select(0);
+          });
+      },
+      error => {
+        console.log(error);
+        const alert = this.alertCtrl.create({
+          title: "There was an error connecting to the printer, please try again!",
+          buttons: [
+            {
+              text: "Ok",
+              handler: () => {
+                load.dismiss();
+                //this.printer.disconnectBluetooth();
+              },
+            },
+          ],
+        });
+        alert.present();
+      },
+    );
   }
 }
