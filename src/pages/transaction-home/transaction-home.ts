d4c05d1e153334f541f6418b7dab9e63a623bab1
Pranay Agarwal <pranayagarwal1208@gmail.com>
2020-02-04 11:51:29 +0530

    Changed getVersionNum to promise

--- src/pages/transaction-home/transaction-home.ts
+++ src/pages/transaction-home/transaction-home.ts
@@ -81,30 +81,32 @@ export class TransactionHomePage {
     this.events.subscribe("cbUpdate:created", async data => {
       await this.getUserData();
     });
-    console.log(this.appVersion.getVersionNumber()); //output - plugin not installed
     axios.get("https://us-central1-open-fintech.cloudfunctions.net/getVersionNumber")
     .then(response => {
       let newestVersion = response.data.versionNumber;
-      if(newestVersion==this.appVersion.getVersionNumber()){
-        this.alertCtrl.create({
-          title: "Update available",
-          subTitle: "Update the app",
-          buttons: [
-            {
-              text: "Not Now",
-              role: "cancel"
-            },
-            {
-              text: "Update now",
-              handler: () => {
-                window.open("https://play.google.com/store/apps/details?id=com.openfintech.openpos", "_system", "location=yes");
+      this.appVersion.getVersionNumber().then(version => {
+        console.log(version);
+        if(newestVersion>version){
+          this.alertCtrl.create({
+            title: "Update available",
+            subTitle: "Update the app",
+            buttons: [
+              {
+                text: "Not Now",
+                role: "cancel"
               },
-            }
-          ]
-        }).present();
-      } else {
-        console.log("Version same");
-      }
+              {
+                text: "Update now",
+                handler: () => {
+                  window.open("https://play.google.com/store/apps/details?id=com.openfintech.openpos", "_system", "location=yes");
+                },
+              }
+            ]
+          }).present();
+        } else {
+          console.log("Version same");
+        }
+      });
     })
     .catch(error=>{
       console.log(error);
