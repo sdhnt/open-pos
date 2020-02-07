12c986acdfa5932929087907fcb463fffc9f8d80
Pranay Agarwal <47204059+pranay1208@users.noreply.github.com>
2020-02-07 19:09:34 +0530

    Slides (#52)
    * Tutorial slides with firebase storage
    
    * Syntax fix
    
    * Preinitialized array error
    
    * Sizing fix
    
    * Zone to array initialization
    
    * length based on lang
    
    * Preload slide
    
    * Slide navigation options + load fix error
    
    * lengthBasedOnlang from firebase
    
    Co-authored-by: Sidhant Gupta <sdhnt@connect.hku.hk>

--- src/pages/help/help.ts
+++ src/pages/help/help.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams, ViewController, App } from "ionic-angular";
+import { Component, ViewChild, NgZone } from "@angular/core";
+import { IonicPage, NavController, NavParams, ViewController, Slides } from "ionic-angular";
 import { DomSanitizer } from "@angular/platform-browser";
 import { StorageProvider } from "../../providers/storage/storage";
 import { TranslateConfigService } from "../../providers/translation/translate-config.service";
+import firebase from 'firebase';
 
 /**
  * Generated class for the HelpPage page.
@@ -20,17 +21,91 @@ export class HelpPage {
   data: any;
   text: any;
   video_set: any;
+  @ViewChild(Slides) slides: Slides;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private view: ViewController,
     private dom: DomSanitizer,
-    private app: App,
     private sp: StorageProvider,
     private translateConfigService: TranslateConfigService,
+    public zone: NgZone
   ) {
     //this.data = this.navParams.get("data");
-    //this.getCoach();
+
+    // this.getCoach();
+    this.currentIndex = 0;
+    this.storageLocation = "gs://open-fintech.appspot.com/tutorial/";
+    let langComponent = this.translateConfigService.getCurrentLanguage();
+    if(langComponent!="en" && langComponent!="my") 
+      langComponent = "en";
+    this.storageLocation+=langComponent+"/Slide";
+  }
+
+  storageLocation: string;
+  hasSlideBeenVisited;
+  currentIndex: number;
+
+  selectSlide(char){
+    if(char=='o'){
+      if(this.currentIndex<=0) this.currentIndex = 0;
+      else if(this.currentIndex>=this.lengthBasedOnLang) this.currentIndex = this.lengthBasedOnLang - 1;
+      this.slides.slideTo(this.currentIndex, 500);
+    } else if(char=='p'){
+      if(this.currentIndex!=0)
+        this.slides.slidePrev(150);
+    } else{
+      if(this.currentIndex<this.lengthBasedOnLang-1)
+        this.slides.slideNext(150);
+    }
+    
+  }
+
+  slideChanged(){
+    this.currentIndex = this.slides.getActiveIndex(); 
+    if(this.currentIndex == this.lengthBasedOnLang-1) this.slides.lockSwipeToNext(true);
+    else this.slides.lockSwipeToNext(false);
+    let img = document.getElementById(this.currentIndex.toString()) as HTMLImageElement
+    if(this.currentIndex==0 || img.src){
+      this.loadNext(this.currentIndex);
+      return;
+    }
+    this.hasSlideBeenVisited[this.currentIndex] = true;
+    let imageString = this.currentIndex.toString()+".JPG";
+    if(this.currentIndex<=9){ //this
+      imageString = "0"+imageString; //get rid of this
+    }//and this too
+    firebase.storage().refFromURL(this.storageLocation+imageString).getDownloadURL()
+      .then(response=>{
+        this.zone.run(()=>{
+          let imageToLoad = document.getElementById(this.currentIndex.toString()) as HTMLImageElement;
+          imageToLoad.src = response;
+        });
+      }).catch(error=>{
+        console.log(error);
+        this.slides.slidePrev(10);
+      });
+    this.loadNext(this.currentIndex);
+  }
+
+  loadNext(index:number){
+    let indexToLoad = index+1;
+    if(this.hasSlideBeenVisited[indexToLoad] || indexToLoad>=this.lengthBasedOnLang){
+      return;
+    }
+    this.hasSlideBeenVisited[indexToLoad] = true;
+    let imageString = indexToLoad.toString()+".JPG";
+    if(indexToLoad<=9){//this too
+      imageString = "0"+imageString; //get rid of this
+    }//and this
+    firebase.storage().refFromURL(this.storageLocation+imageString).getDownloadURL()
+      .then(response=>{
+        let imageToLoad = document.getElementById(indexToLoad.toString()) as HTMLImageElement;
+        imageToLoad.src = response;
+      }).catch(error=>{
+        console.log(error);
+      })
   }
 
   // getCoach() {
@@ -58,6 +133,31 @@ export class HelpPage {
     } else {
       this.openVidVal = i;
     }
+  }
+
+  lengthBasedOnLang: number;
+
+  ionViewWillLoad(){
+    firebase
+      .firestore()
+      .collection("tutorial")
+      .get()
+      .then(doc=>{
+        let map = doc.docs[2].data().langLength;
+        let lang = this.translateConfigService.getCurrentLanguage();
+        if(lang=="en"){
+          this.lengthBasedOnLang = map.en;
+        } else if(lang=="my"){
+          this.lengthBasedOnLang = map.my;
+        } else {
+          this.lengthBasedOnLang = map.en;
+        }
+      })
+    this.zone.run(()=>{
+      this.hasSlideBeenVisited = new Array(this.lengthBasedOnLang).fill(false);
+      this.hasSlideBeenVisited[0] = true;
+      console.log(this.lengthBasedOnLang);
+    })
   }
 
   ionViewDidLoad() {
