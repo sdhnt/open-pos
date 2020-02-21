ddec88918b3f461d48ddeb958bd6985750edde99
Pranay Agarwal <pranayagarwal1208@gmail.com>
2020-02-21 17:30:57 +0530

    Individual contact page functioning

--- src/pages/individual-contact/individual-contact.ts
+++ src/pages/individual-contact/individual-contact.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, AlertController } from "ionic-angular";
 
 /**
  * Generated class for the IndividualContactPage page.
@@ -15,8 +15,15 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
 })
 export class IndividualContactPage {
   listVal = [300, 250, -110, -100, -500];
+  balance: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public alertCtrl: AlertController) {
+      this.balance = 0;
+      this.listVal.forEach(element=>{
+        this.balance+=element;
+      });
+    }
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad IndividualContactPage");
@@ -26,8 +33,38 @@ export class IndividualContactPage {
     this.navCtrl.pop();
   }
 
-  transaction(one:number){
-    //pop up alert
-    //multiply value by *one* to get appropriate -ve,+ve
+  transaction(signedOne: number) {
+    let amountToAdd = 0;
+    const a = this.alertCtrl.create({
+      inputs: [
+        {
+          name: "amount",
+          placeholder: "Amount",
+          type: "number"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+        },
+        {
+          text: "OK",
+          handler: data=>{
+            if(data.amount>0){
+              amountToAdd = data.amount*signedOne;
+            } else console.log("Improper Number");
+          }
+        }
+      ]
+    });
+    a.present();
+    a.onDidDismiss(()=>{
+      if(amountToAdd==0){
+        return;
+      }
+      this.listVal.unshift(amountToAdd);
+      this.balance+=amountToAdd;
+    });
   }
 }
