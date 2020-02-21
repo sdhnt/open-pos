8d6fbd4f65e2e5d7707f3c988515dfdda04c7cd4
seanchok <seanchok@gmail.com>
2020-02-21 20:13:22 +0800

    modularize firestore subcollection methods
    #62

--- src/providers/storage/storage.ts
+++ src/providers/storage/storage.ts
@@ -129,58 +129,6 @@ export class StorageProvider {
 
   backupStorage() {}
 
-  async createFirestoreCollection(db, collectionPath, documents) {
-    const batch = db.batch();
-    documents.forEach(document => {
-      const documentReference = db.collection(collectionPath).doc();
-      batch.set(documentReference, document);
-    });
-    await batch.commit();
-  }
-
-  deleteFirestoreCollection(db, collectionPath, batchSize) {
-    const deleteQueryBatch = (db, query, batchSize, resolve, reject) => {
-      query
-        .get()
-        .then(snapshot => {
-          // When there are no documents left, we are done
-          if (snapshot.size == 0) {
-            return 0;
-          }
-
-          // Delete documents in a batch
-          const batch = db.batch();
-          snapshot.docs.forEach(doc => {
-            batch.delete(doc.ref);
-          });
-
-          return batch.commit().then(() => {
-            return snapshot.size;
-          });
-        })
-        .then(numDeleted => {
-          if (numDeleted === 0) {
-            resolve();
-            return;
-          }
-
-          // Recurse on the next process tick, to avoid
-          // exploding the stack.
-          process.nextTick(() => {
-            deleteQueryBatch(db, query, batchSize, resolve, reject);
-          });
-        })
-        .catch(reject);
-    };
-
-    const collectionRef = db.collection(collectionPath);
-    const query = collectionRef.limit(batchSize);
-
-    return new Promise((resolve, reject) => {
-      deleteQueryBatch(db, query, batchSize, resolve, reject);
-    });
-  }
-
   async setUserDat(user) {
     await this.storage.ready();
     await this.storage.set("user", JSON.stringify(user));
--- src/providers/storage/utilities.ts
+++ src/providers/storage/utilities.ts
@@ -12,3 +12,57 @@ export const queryUser = async (): Promise<{ id?: string; user?: any }> => {
   });
   return dataSet[0];
 };
+
+export const createFirestoreCollection = async (collectionPath, documents) => {
+  const db = firebase.firestore();
+  const batch = db.batch();
+  documents.forEach(document => {
+    const documentReference = db.collection(collectionPath).doc();
+    batch.set(documentReference, document);
+  });
+  await batch.commit();
+};
+
+export const deleteFirestoreCollection = (collectionPath, batchSize) => {
+  const db = firebase.firestore();
+  const deleteQueryBatch = (db, query, batchSize, resolve, reject) => {
+    query
+      .get()
+      .then(snapshot => {
+        // When there are no documents left, we are done
+        if (snapshot.size == 0) {
+          return 0;
+        }
+
+        // Delete documents in a batch
+        const batch = db.batch();
+        snapshot.docs.forEach(doc => {
+          batch.delete(doc.ref);
+        });
+
+        return batch.commit().then(() => {
+          return snapshot.size;
+        });
+      })
+      .then(numDeleted => {
+        if (numDeleted === 0) {
+          resolve();
+          return;
+        }
+
+        // Recurse on the next process tick, to avoid
+        // exploding the stack.
+        process.nextTick(() => {
+          deleteQueryBatch(db, query, batchSize, resolve, reject);
+        });
+      })
+      .catch(reject);
+  };
+
+  const collectionRef = db.collection(collectionPath);
+  const query = collectionRef.limit(batchSize);
+
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(db, query, batchSize, resolve, reject);
+  });
+};
