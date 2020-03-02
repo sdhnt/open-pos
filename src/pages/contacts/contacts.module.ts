63d60af4e5600e16f3d126c6523ee793e808f22e
Sidhant Gupta <sdhnt@connect.hku.hk>
2020-03-02 12:43:21 +0800

    translate half contacts

--- src/pages/contacts/contacts.module.ts
+++ src/pages/contacts/contacts.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { ContactsPage } from "./contacts";
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   declarations: [ContactsPage],
-  imports: [IonicPageModule.forChild(ContactsPage)],
+  imports: [IonicPageModule.forChild(ContactsPage), TranslateModule.forChild()],
+  exports: [ContactsPage]
 })
 export class ContactsPageModule {}
--- src/pages/individual-contact/individual-contact.module.ts
+++ src/pages/individual-contact/individual-contact.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { IndividualContactPage } from "./individual-contact";
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   declarations: [IndividualContactPage],
-  imports: [IonicPageModule.forChild(IndividualContactPage)],
+  imports: [IonicPageModule.forChild(IndividualContactPage), TranslateModule.forChild()],
+  exports: [IndividualContactPage]
 })
 export class IndividualContactPageModule {}
