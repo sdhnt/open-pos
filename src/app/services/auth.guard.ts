0e3152a4c73167c7b004e9ea187dc0608dce5c06
mohitmshah09 <mohitmshah09@gmail.com>
2020-03-24 17:47:40 +0530

    auth guard alert removed

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -15,7 +15,6 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.sp.hasData()) {
-      alert('yes');
       return true;
     }
     this.router.navigate(['/login']);
